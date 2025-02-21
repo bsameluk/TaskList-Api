fa48dd88615cfadeb84b07f765ac63a432e70624
Bohdan Sameliuk <samelukbogd@gmail.com>
2025-02-21 13:19:34 +0100

    update config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -49,7 +49,11 @@ Rails.application.configure do
   # config.assume_ssl = true
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
+  # config.serve_static_assets = true
   config.force_ssl = false
+  config.assume_ssl = false
+
+  Rails.application.routes.default_url_options[:protocol] = "http"
 
   # Log to STDOUT by default
   config.logger = ActiveSupport::Logger.new(STDOUT)
